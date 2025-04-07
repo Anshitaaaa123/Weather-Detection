b44d9f072281902a2ba3570a2de05b772e481edc
Anshitaaaa123 <anshittaaaasingh@gmail.com>
2025-04-07 14:11:44 +0530

    Update github profile

--- src/App.js
+++ src/App.js
@@ -192,7 +192,7 @@ function App() {
 
             <UTCDatetime />
             <Link
-              href="https://github.com/Amin-Awinti"
+              href="https://github.com/Anshitaaaa123/Weather-Detection"
               target="_blank"
               underline="none"
               sx={{ display: 'flex' }}
